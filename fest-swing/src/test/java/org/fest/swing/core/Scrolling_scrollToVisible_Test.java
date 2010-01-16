42489d99cdfe82b7fec2b8b53d545dfde924a639
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-01-16 01:29:51 +0000

    RESOLVED - issue FEST-269: add "requireSelection" in JTableFixture http://jira.codehaus.org/browse/FEST-269
    Minor changes.

--- fest-swing/src/test/java/org/fest/swing/core/Scrolling_scrollToVisible_Test.java
+++ fest-swing/src/test/java/org/fest/swing/core/Scrolling_scrollToVisible_Test.java
@@ -1,15 +1,15 @@
 /*
  * Created on Jan 13, 2009
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2009-2010 the original author or authors.
  */
 package org.fest.swing.core;
@@ -22,10 +22,10 @@ import static org.fest.swing.edt.GuiActionRunner.execute;
 import java.awt.BorderLayout;
 import java.awt.Component;
 
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
-import org.fest.swing.core.Robot;
-import org.fest.swing.driver.Scrolling;
 import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.fixture.JButtonFixture;
 import org.fest.swing.test.core.RobotBasedTestCase;
@@ -34,14 +34,14 @@ import org.junit.Test;
 
 /**
  * Tests for <code>{@link Scrolling#scrollToVisible(Robot, javax.swing.JComponent)}</code>.
- * 
+ *
  * @author Juhos Csaba-Zsolt
  * @author Alex Ruiz
  */
 public class Scrolling_scrollToVisible_Test extends RobotBasedTestCase {
 
   private MyWindow window;
-  
+
   @Override protected void onSetUp() {
     window = MyWindow.createAndShow();
   }
@@ -52,7 +52,7 @@ public class Scrolling_scrollToVisible_Test extends RobotBasedTestCase {
     Scrolling.scrollToVisible(robot, button);
     new JButtonFixture(robot, button).click();
   }
-  
+
   private static class MyWindow extends TestWindow {
     private static final long serialVersionUID = 1L;
 
