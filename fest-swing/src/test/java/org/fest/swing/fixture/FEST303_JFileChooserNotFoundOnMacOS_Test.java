5e988cf82fe3507f6e9bcf38cca3e96a3d9a2d9f
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-03-12 04:21:50 +0000

    IN PROGRESS - issue FEST-303: Unable to find JFileChooserFixture on OSX http://jira.codehaus.org/browse/FEST-303
    Created test case to reproduce bug. It works on Windows. Now time to verify on MacOS.

---
+++ fest-swing/src/test/java/org/fest/swing/fixture/FEST303_JFileChooserNotFoundOnMacOS_Test.java
@@ -0,0 +1,80 @@
+/*
+ * Created on Mar 11, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
+package org.fest.swing.fixture;
+
+import static org.fest.swing.timing.Timeout.timeout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import org.fest.swing.annotation.RunsInCurrentThread;
+import org.fest.swing.annotation.RunsInEDT;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.test.core.RobotBasedTestCase;
+import org.fest.swing.test.swing.TestWindow;
+import org.junit.Test;
+
+/**
+ * Test case for bug <a href="http://jira.codehaus.org/browse/FEST-303" target="_blank">FEST-303</a>.
+ *
+ * @author Alex Ruiz
+ */
+public class FEST303_JFileChooserNotFoundOnMacOS_Test extends RobotBasedTestCase {
+
+  private FrameFixture frame;
+
+  @Override protected void onSetUp() {
+    frame = new FrameFixture(robot, MyWindow.createNew());
+    frame.show();
+  }
+
+  @Test
+  public void should_find_JFileChooser() {
+    frame.button().click();
+    frame.fileChooser(timeout(5000));
+  }
+
+  private static class MyWindow extends TestWindow {
+    private static final long serialVersionUID = 1L;
+
+    final JButton button = new JButton("Click me");
+    final JFileChooser chooser = new JFileChooser();
+
+    @RunsInEDT
+    static MyWindow createNew() {
+      return GuiActionRunner.execute(new GuiQuery<MyWindow>() {
+        protected MyWindow executeInEDT() {
+          return new MyWindow();
+        }
+      });
+    }
+
+    @RunsInCurrentThread
+    private MyWindow() {
+      super(FEST303_JFileChooserNotFoundOnMacOS_Test.class);
+      add(button);
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          chooser.showOpenDialog(MyWindow.this);
+        }
+      });
+    }
+  }
+}
