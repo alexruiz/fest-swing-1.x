8d0d84818ff5c63c0cc2748acb074060d9d1b3c9
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-04-02 18:21:52 +0000

    [FEST-319] UI for easy creation of keyboard mappings. http://jira.codehaus.org/browse/FEST-319
    Initial import.

---
+++ fest-keyboard-mapping/src/main/java/org/fest/keyboard/mapping/Application.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Apr 1, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
+package org.fest.keyboard.mapping;
+
+import static javax.swing.UIManager.*;
+import static javax.swing.SwingUtilities.*;
+
+/**
+ * Understands the application's starting point
+ *
+ * @author Alex Ruiz
+ */
+public class Application {
+
+  public static void main(String... args) {
+    invokeLater(new Runnable() {
+      @Override public void run() {
+        setLaF();
+        new MainFrame().setVisible(true);
+      }
+
+      private void setLaF() {
+        try {
+          setLookAndFeel(getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+          ignored.printStackTrace();
+        }
+      }
+    });
+  }
+}
