33fdb8fda0255f7a8e7f89d99b507cd063001443
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-05-24 14:05:20 +0000

    REOPENED - issue FEST-83: Create NodeFormatter to print Node properties in assertion errors http://jira.codehaus.org/browse/FEST-83
    Cleaning up code.

--- fest-javafx-desktop/src/main/java/org/fest/javafx/format/NodeFormatterTemplate.java
+++ fest-javafx-desktop/src/main/java/org/fest/javafx/format/NodeFormatterTemplate.java
@@ -15,37 +15,99 @@
  */
 package org.fest.javafx.format;
 
-import org.fest.javafx.annotations.RunsInCurrentThread;
+import java.util.*;
 
+import org.fest.javafx.annotations.RunsInCurrentThread;
 import javafx.scene.Node;
 
+import static java.lang.String.valueOf;
+import static java.util.Collections.emptyList;
 import static org.fest.util.Strings.concat;
+import static org.fest.util.Strings.quote;
 
 /**
- * Understands a template for implementations of <code>{@link NodeFormatter}</code>.
+ * Understands a basic implementation of <code>{@link NodeFormatter}</code>.
  *
  * @author Alex Ruiz
  */
-public abstract class NodeFormatterTemplate implements NodeFormatter {
+public class NodeFormatterTemplate implements NodeFormatter {
 
-  /** {@inheritDoc} */
+  /**
+   * Returns the {@code String} representation of the given <code>{@link Node}</code>. This implementation only
+   * includes the node's id, 'disabled' and 'visible' properties. To add more properties simply override
+   * <code>{@link #formattedPropertiesOf(Node)}</code> returning one property per {@code String} in the format:
+   * <pre>
+   * propertyName=propertyValue
+   * </pre>
+   * @param n the given {@code Node}.
+   * @return the {@code String} representation of the given {@code Node}.
+   * @throws NullPointerException if the given {@code Node} is <code>null</code>.
+   * @throws IllegalArgumentException if the type of the given {@code Node} is not supported by this formatter.
+   */
   @RunsInCurrentThread
   @Override public final String format(Node n) {
     validateTypeOf(n);
     return doFormat(n);
   }
 
+  @RunsInCurrentThread
+  private String doFormat(Node n) {
+    StringBuilder b = new StringBuilder();
+    b.append(n.getClass().getName()).append("[");
+    String[] properties = propertiesOf(n);
+    int propertyCount = properties.length;
+    for (int i = 0; i < propertyCount; i++) {
+      b.append(properties[i]);
+      if (i != propertyCount - 1) b.append(", ");
+    }
+    b.append("]");
+    return b.toString();
+  }
+
+  @RunsInCurrentThread
+  private String[] propertiesOf(Node n) {
+    List<String> properties = new ArrayList<String>();
+    properties.add(property("id", n.get$id()));
+    properties.addAll(formattedPropertiesOf(n));
+    properties.add(property("disabled", n.get$disabled()));
+    properties.add(property("visible", n.get$visible()));
+    return properties.toArray(new String[properties.size()]);
+  }
+
+  private String property(String name, Object value) {
+    return concat(name, "=", quote(value));
+  }
+
+  private String property(String name, boolean value) {
+    return concat(name, "=", valueOf(value));
+  }
+
   /**
-   * Returns the {@code String} representation of the given <code>{@link Node}</code>.
+   * Returns the formatted properties of the given <code>{@link Node}</code> to be included in
+   * <code>{@link #format(Node)}</code>. Each {@code String} represents a property in the format:
+   * <pre>
+   * propertyName=propertyValue
+   * </pre>
    * @param n the given {@code Node}.
-   * @return the {@code String} representation of the given {@code Node}.
+   * @return the formatted properties to be included in <code>{code format(Node)}</code>.
    */
   @RunsInCurrentThread
-  protected abstract String doFormat(Node n);
+  protected Collection<String> formattedPropertiesOf(Node n) {
+    return emptyList();
+  }
 
   private void validateTypeOf(Node n) {
     if (n == null) throw new NullPointerException("The node to format should not be null");
     if (!targetType().isAssignableFrom(n.getClass()))
       throw new IllegalArgumentException(concat("This formatter only supports components of type ", targetType().getName()));
+  }
+
+  /**
+   * Returns <code>{@link Node}</code>.{@code class}.
+   * @return {@code Node.class}.
+   * @see NodeFormatter#targetType()
+   */
+  @Override public Class<? extends Node> targetType() {
+    return Node.class;
   }
 }
--- fest-javafx-desktop/src/test/java/org/fest/javafx/format/NodeFormatterTemplate_format_Test.java
+++ fest-javafx-desktop/src/test/java/org/fest/javafx/format/NodeFormatterTemplate_format_Test.java
@@ -1,35 +1,32 @@
 /*
  * Created on May 21, 2010
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2010 the original author or authors.
  */
 package org.fest.javafx.format;
 
-import java.lang.reflect.Method;
-
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 import org.junit.*;
 
-import org.fest.mocks.EasyMockTemplate;
 import org.fest.test.CodeToTest;
 
 import static org.easymock.EasyMock.expect;
 import static org.easymock.classextension.EasyMock.createMock;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.javafx.test.node.Nodes.node;
 import static org.fest.test.ExpectedFailure.expect;
 import static org.fest.util.Strings.concat;
 
@@ -40,65 +37,31 @@ import static org.fest.util.Strings.concat;
  */
 public class NodeFormatterTemplate_format_Test {
 
-  private static Method doFormatMethod;
-  private ConcreteNodeFormatter formatter;
-  
-  @BeforeClass
-  public static void setUpOnce() throws Exception {
-    doFormatMethod = ConcreteNodeFormatter.class.getDeclaredMethod("doFormat", Node.class);
-  }
-  
+  private NodeFormatterTemplate formatter;
+
   @Before
   public void setUp() {
-    formatter = createMock(ConcreteNodeFormatter.class, doFormatMethod);
+    formatter = new NodeFormatterTemplate();
   }
-  
+
   @Test
   public void should_format_node_only_if_node_type_is_supported() {
-    final String formattedNode = "Hello World!";
-    final Node node = createMock(Button.class);
-    formatter.updateTargetType(node.getClass());
-    new EasyMockTemplate(formatter) {
-      @Override protected void expectations() {
-        expect(formatter.doFormat(node)).andReturn(formattedNode);
-      }
-      
-      @Override protected void codeToTest() {
-        assertThat(formatter.format(node)).isEqualTo(formattedNode);
-      }
-    };
+    String formatted = formatter.format(node().withId("MyNode").createNew());
+    assertThat(formatted).isEqualTo("org.fest.javafx.core.ConcreteNode[id='MyNode', disabled=false, visible=true]");
   }
-  
+
   @Test(expected = NullPointerException.class)
   public void should_throw_error_if_node_is_null() {
     formatter.format(null);
   }
-  
+
   @Test
   public void should_throw_error_if_node_type_is_not_supported() {
-    Class<? extends Node> supportedType = Button.class;
-    formatter.updateTargetType(supportedType);
-    String message = concat("This formatter only supports components of type ", supportedType.getName());
+    String message = concat("This formatter only supports components of type ", Node.class.getName());
     expect(IllegalArgumentException.class).withMessage(message).on(new CodeToTest() {
       @Override public void run() {
         formatter.format(createMock(Text.class));
       }
-    }); 
-  }
-  
-  private static class ConcreteNodeFormatter extends NodeFormatterTemplate {
-    private Class<? extends Node> targetType;
-
-    @Override protected String doFormat(Node n) {
-      return null;
-    }
-    
-    void updateTargetType(Class<? extends Node> newTargetType) {
-      targetType = newTargetType;
-    }
-
-    @Override public Class<? extends Node> targetType() {
-      return targetType;
-    }
+    });
   }
 }
