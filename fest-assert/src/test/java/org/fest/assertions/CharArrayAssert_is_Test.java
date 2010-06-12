89786854d474eb2b888cf4a064c903bc32dd6492
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-06-12 00:15:01 +0000

    Code cleanup.

--- fest-assert/src/test/java/org/fest/assertions/CharArrayAssert_is_Test.java
+++ fest-assert/src/test/java/org/fest/assertions/CharArrayAssert_is_Test.java
@@ -14,101 +14,20 @@
  */
 package org.fest.assertions;
 
-import static org.fest.assertions.CommonFailures.*;
 import static org.fest.assertions.EmptyArrays.emptyCharArray;
-import static org.fest.assertions.NotNull.notNullCharArray;
-import static org.fest.test.ExpectedFailure.expectAssertionError;
-
-import org.fest.test.CodeToTest;
-import org.junit.Test;
 
 /**
  * Tests for <code>{@link CharArrayAssert#is(Condition)}</code>.
  *
  * @author Alex Ruiz
  */
-public class CharArrayAssert_is_Test implements GenericAssert_satisfies_orAlias_TestCase {
+public class CharArrayAssert_is_Test extends GenericAssert_is_TestCase<char[]> {
 
-  @Test
-  public void should_pass_if_condition_is_satisfied() {
-    new CharArrayAssert(emptyCharArray()).is(notNullCharArray());
+  protected CharArrayAssert assertObject() {
+    return new CharArrayAssert(emptyCharArray());
   }
 
-  @Test
-  public void should_throw_error_if_condition_is_null() {
-    expectErrorIfConditionIsNull().on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(emptyCharArray()).is(null);
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_if_condition_is_not_satisfied() {
-    expectAssertionError("actual value:<null> should be:<NotNull>").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).is(notNullCharArray());
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_and_display_description_of_assertion_if_condition_is_not_satisfied() {
-    expectAssertionError("[A Test] actual value:<null> should be:<NotNull>").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).as("A Test")
-                                 .is(notNullCharArray());
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_and_display_description_of_condition_if_condition_is_not_satisfied() {
-    expectAssertionError("actual value:<null> should be:<Not Null>").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).is(notNullCharArray().as("Not Null"));
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_and_display_descriptions_of_assertion_and_condition_if_condition_is_not_satisfied() {
-    expectAssertionError("[A Test] actual value:<null> should be:<Not Null>").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).as("A Test")
-                                 .is(notNullCharArray().as("Not Null"));
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_with_custom_message_if_condition_is_not_satisfied() {
-    expectAssertionError("My custom message").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).overridingErrorMessage("My custom message")
-                                 .is(notNullCharArray());
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_with_custom_message_ignoring_description_of_assertion_if_condition_is_not_satisfied() {
-    expectAssertionError("My custom message").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).as("A Test")
-                                 .overridingErrorMessage("My custom message")
-                                 .is(notNullCharArray());
-      }
-    });
-  }
-
-  @Test
-  public void should_fail_with_custom_message_ignoring_description_of_condition_if_condition_is_not_satisfied() {
-    expectAssertionError("My custom message").on(new CodeToTest() {
-      public void run() {
-        new CharArrayAssert(null).overridingErrorMessage("My custom message")
-                                 .is(notNullCharArray().as("Not Null"));
-      }
-    });
+  protected CharArrayAssert assertObjectWithNullTarget() {
+    return new CharArrayAssert(null);
   }
 }
