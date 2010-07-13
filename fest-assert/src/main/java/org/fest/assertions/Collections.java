7df2837d09ef8c06bfb770adee1f5a7e86daac76
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-07-13 13:27:01 +0000

    * Updated copyright year. * Added missing file headers and class-level Javadocs.

--- fest-assert/src/main/java/org/fest/assertions/Collections.java
+++ fest-assert/src/main/java/org/fest/assertions/Collections.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  * 
- * Copyright @2008-2009 the original author or authors.
+ * Copyright @2008-2010 the original author or authors.
  */
 package org.fest.assertions;
 
--- fest-assert/src/test/java/org/fest/assertions/CollectionAssert_onProperty_Test.java
+++ fest-assert/src/test/java/org/fest/assertions/CollectionAssert_onProperty_Test.java
@@ -1,7 +1,22 @@
+/*
+ * Created on May 26, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
 package org.fest.assertions;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.fest.assertions.CommonFailures.expectErrorIfActualIsNull;
+import static org.fest.assertions.Title.*;
 
 import java.util.*;
 
@@ -9,6 +24,11 @@ import org.fest.test.CodeToTest;
 import org.fest.util.IntrospectionError;
 import org.junit.*;
 
+/**
+ * Tests for <code>{@link CollectionAssert#onProperty(String)}</code>.
+ *
+ * @author Joel Costigliola
+ */
 public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test {
 
   private static Collection<Person> friends;
@@ -34,7 +54,7 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     friends.add(pier);
     Person paula = new Person(2L, "Paula", 32, 67L, false, 'O', (byte) 2, (short) 1975, 1.80f, 90.2, "Madrid");
     paula.setFather(new Person(22L, "PaulaFather", 62, 67L, false, 'O', (byte) 2, (short) 1975, 1.80f, 90.2, "Madrid"));
-    paula.setTitle(Title.Miss);
+    paula.setTitle(Miss);
     friends.add(paula);
     Person jack = new Person(3L, "Jack", 16, 678L, false, 'J', (byte) 4, (short) 1976, 1.70f, 100.3, "London");
     jack.setFather(new Person(33L, "JackFather", 46, 678L, false, 'J', (byte) 4, (short) 1976, 1.70f, 100.3, "London"));
@@ -44,69 +64,58 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     friends.add(jack2);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_non_primitive_type_property() {
     assertThat(friends).onProperty("id").contains(1L, 2L, 4L);
     assertThat(friends).onProperty("homeTown").contains("Paris", "Roma", "London", "Madrid");
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_enum_type_property() {
-    assertThat(friends).onProperty("title").contains(Title.Mr, Title.Miss);
+    assertThat(friends).onProperty("title").contains(Mr, Miss);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_int_property() {
     assertThat(friends).onProperty("age").containsOnly(25, 16, 44, 32);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_long_property() {
     assertThat(friends).onProperty("socialSecurityNumber").containsOnly(6L, 67L, 678L, 6789L);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_short_property() {
-    assertThat(friends).onProperty("yearOfBirth").containsOnly((short) 1974, (short) 1975, (short) 1976, (short) 1977);
+    assertThat(friends).onProperty("yearOfBirth").containsOnly((short)1974, (short)1975, (short)1976, (short)1977);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_float_property() {
     assertThat(friends).onProperty("height").containsOnly(1.90f, 1.80f, 1.70f, 1.60f);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_double_property() {
     assertThat(friends).onProperty("weight").containsOnly(80.1, 90.2, 100.3, 110.4);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_boolean_property() {
     assertThat(friends).onProperty("male").containsOnly(true, false, false, true);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_byte_property() {
-    assertThat(friends).onProperty("favoriteByte").containsOnly((byte) 1, (byte) 2, (byte) 4, (byte) 8);
+    assertThat(friends).onProperty("favoriteByte").containsOnly((byte)1, (byte)2, (byte)4, (byte)8);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_char_property() {
     assertThat(friends).onProperty("favoriteAlphabetLetter").contains('P', 'K');
     assertThat(friends).onProperty("favoriteAlphabetLetter").containsOnly('O', 'J', 'P', 'K');
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_non_primitive_type_nested_property() {
     assertThat(friends).onProperty("name.firstName").contains("Pier", "Paula", "Jack");
@@ -114,19 +123,16 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
         "JackFather2");
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_enum_type_nested_property() {
-    assertThat(friends).onProperty("father.title").containsOnly(Title.Mr, Title.Mr, Title.Mr, Title.Mr);
+    assertThat(friends).onProperty("father.title").containsOnly(Mr, Mr, Mr, Mr);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_on_primitive_type_nested_property() {
     assertThat(friends).onProperty("father.age").containsOnly(55, 46, 74, 62);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_fail_on_non_primitive_type_property() {
     try {
@@ -137,7 +143,6 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     }
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_fail_on_non_primitive_type_nested_property() {
     try {
@@ -150,23 +155,21 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
   @Test
   public void should_fail_on_enum_type_property() {
     try {
-      assertThat(friends).onProperty("title").contains(Title.Mr, Title.Ms);
+      assertThat(friends).onProperty("title").contains(Mr, Ms);
     } catch (AssertionError e) {
       assertThat(e).hasMessage("<[Mr, Miss, Mr, Mr]> does not contain element(s):<[Ms]>");
     }
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_fail_on_enum_type_nested_property() {
     try {
-      assertThat(friends).onProperty("father.title").contains(Title.Miss);
+      assertThat(friends).onProperty("father.title").contains(Miss);
     } catch (AssertionError e) {
       assertThat(e).hasMessage("<[Mr, Mr, Mr, Mr]> does not contain element(s):<[Miss]>");
     }
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_fail_on_primitive_type_property() {
     try {
@@ -176,7 +179,6 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     }
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_fail_on_primitive_type_nested_property() {
     try {
@@ -186,7 +188,6 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     }
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_fail_because_of_unknown_property() {
     try {
@@ -197,7 +198,6 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     }
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_even_if_actual_contains_null_elements() {
     friends.add(null);
@@ -205,14 +205,12 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
         "JackFather2");
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_with_null_nested_property_in_actual_elements() {
     friends.iterator().next().setFather(null);
     assertThat(friends).onProperty("father.name.firstName").containsOnly("PaulaFather", "JackFather", "JackFather2");
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_with_null_property_in_actual_elements() {
     for (Person person : friends) {
@@ -229,7 +227,6 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
     assertThat(friends).onProperty("id").containsOnly(null, null, null, null);
   }
 
-  /** {@inheritDoc} */
   @Test
   public void should_pass_if_actual_is_empty() {
     List<Person> emptyList = new ArrayList<Person>();
@@ -244,5 +241,4 @@ public class CollectionAssert_onProperty_Test implements Assert_onProperty_Test 
       }
     });
   }
-
 }
