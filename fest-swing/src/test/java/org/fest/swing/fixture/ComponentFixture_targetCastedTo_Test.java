76992494a6c6ce2016cc491c2fb10913906ef06c
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-07-28 06:42:34 +0000

    IN PROGRESS - issue FEST-372: I'm missing a .enabled() for JRadioButtonFixture - and other components http://jira.codehaus.org/browse/FEST-372
    Cleaning up code before implementing feature.

---
+++ fest-swing/src/test/java/org/fest/swing/fixture/ComponentFixture_targetCastedTo_Test.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Jul 27, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
+package org.fest.swing.fixture;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import javax.swing.JTextField;
+
+import org.junit.Test;
+
+/**
+ * Tests for <code>{@link ComponentFixture#targetCastedTo(Class)}</code>.
+ *
+ * @author Alex Ruiz
+ */
+public class ComponentFixture_targetCastedTo_Test extends ComponentFixture_TestCase {
+
+  @Test
+  public void should_cast_target_Component() {
+    JTextField castedTarget = fixture.targetCastedTo(JTextField.class);
+    assertThat(castedTarget).isSameAs(target);
+  }
+}
