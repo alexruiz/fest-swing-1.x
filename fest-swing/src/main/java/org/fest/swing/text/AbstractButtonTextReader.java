f613d381cd016a65bc785274840ff64a17363510
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-07-30 04:00:02 +0000

    IN PROGRESS - issue FEST-316: Supporting 'Is Text Present' API on a Dialog or Frame http://jira.codehaus.org/browse/FEST-316
    Added support for reading text from AbstractButtons.

---
+++ fest-swing/src/main/java/org/fest/swing/text/AbstractButtonTextReader.java
@@ -0,0 +1,52 @@
+/*
+ * Created on Jul 29, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
+package org.fest.swing.text;
+
+import javax.swing.AbstractButton;
+
+import org.fest.swing.annotation.RunsInCurrentThread;
+
+/**
+ * Understands reading the text displayed in a <code>{@link AbstractButton}</code>.
+ *
+ * @author Alex Ruiz
+ *
+ * @since 1.3
+ */
+public class AbstractButtonTextReader implements TextReader<AbstractButton> {
+
+  /**
+   * Indicates that this reader supports {@code AbstractButton}s.
+   * @return {@code AbstractButton.class}.
+   */
+  public Class<AbstractButton> supportedComponent() {
+    return AbstractButton.class;
+  }
+
+  /**
+   * Indicates whether the given {@code AbstractButton} displays the given text.
+   * @param button the given {@code AbstractButton}.
+   * @param text the given text.
+   * @return {@code true} if the given {@code AbstractButton} displays the given text; {@code false} otherwise.
+   */
+  @RunsInCurrentThread
+  public boolean containsText(AbstractButton button, String text) {
+    String labelText = button.getText();
+    if (labelText == null) return false;
+    return labelText.contains(text);
+  }
+
+}
