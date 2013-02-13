9fe9aa5cff1ff3dbb77af4d774b400f44f97027a
Alex Ruiz <alruiz@google.com>
2013-02-13 04:44:45 -0800

    Fixed tests.

--- fest-swing/src/main/java/org/fest/swing/core/matcher/DialogMatcher.java
+++ fest-swing/src/main/java/org/fest/swing/core/matcher/DialogMatcher.java
@@ -1,15 +1,15 @@
 /*
  * Created on Jul 16, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2008-2013 the original author or authors.
  */
 package org.fest.swing.core.matcher;
@@ -24,7 +24,7 @@ import org.fest.swing.annotation.RunsInCurrentThread;
 
 /**
  * Matches AWT or Swing {@code Dialog}s by name, title or visibility on the screen.
- * 
+ *
  * @author Alex Ruiz
  */
 public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
@@ -34,14 +34,14 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
    * <p>
    * Creates a new {@link DialogMatcher} that matches an AWT or Swing {@code Dialog} by name.
    * </p>
-   * 
+   *
    * <p>
    * The following code listing shows how to match a {@code Dialog} by name and title:
    * <pre>
    * DialogMatcher m = {@link #withName(String) withName}("saveFile").{@link #andTitle(String) andTitle}("Save File");
    * </pre>
    * </p>
-   * 
+   *
    * <p>
    * The following code listing shows how to match a {@code Dialog}, that should be showing on the screen, by name and
    * title:
@@ -49,26 +49,26 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
    * DialogMatcher m = {@link #withName(String) withName}("saveFile").{@link #andTitle(String) andTitle}("Save File").{@link #andShowing() andShowing}();
    * </pre>
    * </p>
-   * 
+   *
    * @param name the id to match.
    * @return the created matcher.
    */
   public static DialogMatcher withName(@Nullable String name) {
-    return new DialogMatcher(name, any());
+    return new DialogMatcher(name, anyValue());
   }
 
   /**
    * <p>
    * Creates a new {@link DialogMatcher} that matches an AWT or Swing {@code Dialog} by title.
    * </p>
-   * 
+   *
    * <p>
    * The following code listing shows how to match a {@code Dialog} title:
    * <pre>
    * DialogMatcher m = {@link #withTitle(String) withTitle}("Save File");
    * </pre>
    * </p>
-   * 
+   *
    * <p>
    * The following code listing shows how to match a {@code Dialog}, that should be showing on the screen,
    * by title:
@@ -76,26 +76,26 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
    * DialogMatcher m = {@link #withTitle(String) withTitle}("Save File").{@link #andShowing() andShowing}();
    * </pre>
    * </p>
-   * 
+   *
    * @param title the title to match. It can be a regular expression.
    * @return the created matcher.
    */
   public static @Nonnull DialogMatcher withTitle(@Nullable String title) {
-    return new DialogMatcher(any(), title);
+    return new DialogMatcher(anyValue(), title);
   }
 
   /**
    * <p>
    * Creates a new {@link DialogMatcher} that matches an AWT or Swing {@code Dialog} by title.
    * </p>
-   * 
+   *
    * <p>
    * The following code listing shows how to match a {@code Dialog} title, using a regular expression pattern:
    * <pre>
    * DialogMatcher m = {@link #withTitle(Pattern) withTitle}(Pattern.compile("Sav.*"));
    * </pre>
    * </p>
-   * 
+   *
    * <p>
    * The following code listing shows how to match a {@code Dialog}, that should be showing on the screen, by
    * title, using a regular expression pattern:
@@ -103,7 +103,7 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
    * DialogMatcher m = {@link #withTitle(Pattern) withTitle}(Pattern.compile("Sav.*")).{@link #andShowing() andShowing}();
    * </pre>
    * </p>
-   * 
+   *
    * @param pattern the regular expression pattern to match.
    * @return the created matcher.
    * @since 1.2
@@ -114,7 +114,7 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
 
   /**
    * Creates a new {@link DialogMatcher} that matches any {@code Dialog}.
-   * 
+   *
    * @return the created matcher.
    */
   public static @Nonnull DialogMatcher any() {
@@ -129,7 +129,7 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
   /**
    * Specifies the title to match. If this matcher was created using {@link #withTitle(String)} or
    * {@link #withTitle(Pattern)}, this method will simply update the title to match.
-   * 
+   *
    * @param newTitle the new title to match. It can be a regular expression.
    * @return this matcher.
    */
@@ -141,7 +141,7 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
   /**
    * Specifies the title to match. If this matcher was created using {@link #withTitle(String)}, or
    * {@link #withTitle(Pattern)} this method will simply update the title to match.
-   * 
+   *
    * @param titlePattern the regular expression pattern to match.
    * @return this matcher.
    * @since 1.2
@@ -153,7 +153,7 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
 
   /**
    * Indicates that the AWT or Swing {@code Dialog} to match should be showing on the screen.
-   * 
+   *
    * @return this matcher.
    */
   public @Nonnull DialogMatcher andShowing() {
@@ -165,12 +165,12 @@ public final class DialogMatcher extends NamedComponentMatcherTemplate<Dialog> {
    * <p>
    * Indicates whether the name and title of the given AWT or Swing {@code Dialog} match the ones in this matcher.
    * </p>
-   * 
+   *
    * <p>
    * <b>Note:</b> This method is accessed in the current executing thread. Such thread may or may not be the event
    * dispatch thread (EDT.) Client code must call this method from the EDT.
    * </p>
-   * 
+   *
    * @param dialog the {@code Dialog} to match.
    * @return {@code true} if the {@code Dialog} matches the search criteria in this matcher.
    */
--- fest-swing/src/main/java/org/fest/swing/core/matcher/NamedComponentMatcherTemplate.java
+++ fest-swing/src/main/java/org/fest/swing/core/matcher/NamedComponentMatcherTemplate.java
@@ -1,15 +1,15 @@
 /*
  * Created on Jan 12, 2009
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2009-2013 the original author or authors.
  */
 package org.fest.swing.core.matcher;
@@ -30,9 +30,9 @@ import org.fest.swing.core.GenericTypeMatcher;
 /**
  * Template for matching AWT or Swing {@code Component} by name. Subclasses are free to add other properties to use as
  * search criteria.
- * 
+ *
  * @param <T> the type of {@code Component} supported by this matcher.
- * 
+ *
  * @author Alex Ruiz
  */
 public abstract class NamedComponentMatcherTemplate<T extends Component> extends GenericTypeMatcher<T> {
@@ -51,7 +51,7 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
 
   /**
    * Creates a new {@link NamedComponentMatcherTemplate}.
-   * 
+   *
    * @param supportedType the type supported by this matcher.
    * @throws NullPointerException if the given type is {@code null}.
    */
@@ -62,7 +62,7 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
 
   /**
    * Creates a new {@link NamedComponentMatcherTemplate}.
-   * 
+   *
    * @param supportedType the type supported by this matcher.
    * @param name the name to match.
    * @throws NullPointerException if the given type is {@code null}.
@@ -75,7 +75,7 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
   /**
    * Returns the name to match surrounded by double quotes. If the name to match has not been set, it will return
    * {@link #anyValue()}. This method is commonly used in implementations of {@code toString}.
-   * 
+   *
    * @return the component name to match surrounded by double quotes, or {@link #anyValue()} if the name to match has
    *         not been set.
    */
@@ -86,7 +86,7 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
   /**
    * Returns the given property value to match surrounded by double quotes. If the property has not been set, it will
    * return {@link #anyValue()}. This method is commonly used in implementations of {@code toString}.
-   * 
+   *
    * @param propertyValue the given property value.
    * @return the given property value to match surrounded by double quotes, or {@link #anyValue()} if the property value
    *         has not been set.
@@ -105,7 +105,7 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
   /**
    * Indicates whether the given value matches the name in this matcher. It always returns {@code true} if this
    * matcher's name is {@link #anyValue()}.
-   * 
+   *
    * @param actual the actual name of an AWT or Swing {@code Component}.
    * @return {@code true} if this matcher's name is {@code ANY} or if both the actual name is equal to the one in this
    *         matcher. Otherwise {@code false}.
@@ -127,7 +127,7 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
    * matching is performed</li>
    * <li>otherwise, it checks that both the expected and actual values are equal</li>
    * </ol>
-   * 
+   *
    * @param expected the expected value in this matcher.
    * @param actual the actual property value.
    * @return {@code true} if the values match, otherwise {@code false}.
@@ -143,6 +143,10 @@ public abstract class NamedComponentMatcherTemplate<T extends Component> extends
       return match((Pattern) expected, (CharSequence) actual);
     }
     return areEqual(expected, actual);
+  }
+
+  protected final @Nullable Object name() {
+    return name;
   }
 
   protected static final @Nonnull Object anyValue() {
