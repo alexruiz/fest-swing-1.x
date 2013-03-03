202ca39be190366226aeb364ecbe2d8f9c74e543
Alex Ruiz <alex.ruiz.05@gmail.com>
2013-03-02 16:37:01 -0800

    Replacing package.html with package-info.java.

---
+++ fest-swing/src/main/java/org/fest/swing/core/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Created on Mar 2, 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2013 the original author or authors.
+ */
+/**
+ * Low-level components that enable functional testing of AWT and Swing {@code Component}s.
+ *
+ * <p>
+ * Most of the classes of in this packages were adapted from <a href="http://abbot.sourceforge.net"
+ * target="_blank">Abbot</a>. Many thanks to Timothy Wall for allowing us to use them.
+ * </p>
+ *
+ * @author Alex Ruiz
+ */
+package org.fest.swing.core;
---
+++ fest-swing/src/main/java/org/fest/swing/driver/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * Created on Mar 2, 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2013 the original author or authors.
+ */
+/**
+ * <p>
+ * {@Component}-specific test drivers. Classes in this package are not part of the API. Use the classes in
+ * {@link org.fest.swing.fixture} in your tests.
+ * </p>
+ *
+ * <p>
+ * A test driver is capable of:
+ * <ul>
+ * <li>Simulate user input on a {@code Component}</li>
+ * <li>Verify the state of a {@code Component}</li>
+ * <li>Safely expose the state of a {@code Component}</li>
+ * </p>
+ *
+ * @author Alex Ruiz
+ */
+package org.fest.swing.driver;
